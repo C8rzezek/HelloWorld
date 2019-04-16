4ec5f8e299c2a1877b5aa83be450c0d5fd896fa3
C8rzezek <cezary.brzezek1@gmai.com>
2019-04-16 21:29:00 +0200

    Secondal commit

--- src/main/java/com/example/helloword/HellowordApplication.java
+++ src/main/java/com/example/helloword/HellowordApplication.java
@@ -2,13 +2,19 @@ package com.example.helloword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HellowordApplication {
+public class HellowordApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HellowordApplication.class, args);
         System.out.println("czexscx");
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HellowordApplication.class);
     }
 
 }
